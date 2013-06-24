de9c18ca51cc8b1305b2f09b3a1313b6c1086b41
gerdemb <github@ibeni.net>
2013-06-24 11:49:32 +0200

    Setup travis-ci

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='djangobb_forum',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-            'django>=1.3.1',
+            'django>=1.5.1',
             'pil>=1.1.7',
             'django-haystack',
             'django-pagination',
